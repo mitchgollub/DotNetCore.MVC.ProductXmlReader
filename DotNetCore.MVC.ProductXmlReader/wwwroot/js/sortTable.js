e26dbe0f5ab24049d4e1130d383c6d1b0cd33402
Mitch Gollub <mgollub@live.com>
2018-11-03 12:06:51 -0400

    Removed Header/Footer, only sort ascending

--- DotNetCore.MVC.ProductXmlReader/wwwroot/js/sortTable.js
+++ DotNetCore.MVC.ProductXmlReader/wwwroot/js/sortTable.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    $.fn.dataTable.defaults.column.asSorting = ['asc'];
     var table = $('#table').DataTable({
         "order": [[0, "asc"]],
         "fnRowCallback": function (nRow, aData, ) {
